297cbde90c6fb20cb63ec2be2d9552f55d787793
unknown <abishek.vp13gmail.com>
2023-12-26 00:27:52 +0530

    update 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function image_data(){
+function preview(){
     var headlinedata=document.getElementById('inpheadline').value;
     var contentdata=document.getElementById('inpcontent').value;
     var taglinedata=document.getElementById('inptagline').value;
@@ -8,14 +8,15 @@ function image_data(){
     headline.innerHTML=headlinedata;
     content.innerHTML=contentdata;
     tagline.innerHTML=taglinedata;
+    document.getElementById('extract').addEventListener('click', function () {
+        html2canvas(document.getElementById('image')).then(function (canvas) {
+            var imageData = canvas.toDataURL('image/jpeg');
+            var link = document.createElement('a');
+            link.href = imageData;
+            link.download = 'Image.jpg';
+            link.click();
+        });
+    });
 }
 
-document.getElementById('extract').addEventListener('click', function () {
-    html2canvas(document.getElementById('image')).then(function (canvas) {
-        var imageData = canvas.toDataURL('image/jpeg');
-        var link = document.createElement('a');
-        link.href = imageData;
-        link.download = 'Image.jpg';
-        link.click();
-    });
-});
+
