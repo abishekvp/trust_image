e9342f318e9184919a9936da6d6bbdd3ecc5067d
abishekvp <abisihek.vp13@gmail.com>
2024-01-07 20:05:14 +0530

    template finish

--- js/index.js
+++ js/index.js
@@ -1,74 +1,57 @@
 
 function content(){
-    var headlinedata=document.getElementById('inpheadline').value;
-    var contentdata1=document.getElementById('inpcontent1').value;
-    var contentdata2=document.getElementById('inpcontent2').value;
-    var contentdata3=document.getElementById('inpcontent3').value;
+  var placedata=document.getElementById('inpplace').value;
+  var headlinedata=document.getElementById('inpheadline').value;
+  var contentdata1=document.getElementById('inpcontent1').value;
+  var contentdata2=document.getElementById('inpcontent2').value;
+  var contentdata3=document.getElementById('inpcontent3').value;
+  var contentdata4=document.getElementById('inpcontent4').value;
+  var contactdata=document.getElementById('inpad_contact').value;
+  var datedata=document.getElementById('inpdate').value;
 
-    var headline=document.getElementById('headline');
-    var content1=document.getElementById('content1');
-    var content2=document.getElementById('content2');
-    var content3=document.getElementById('content3');
-    
-    var headline_color=document.getElementById('color_headline').value;
-    var color_content1=document.getElementById('color_content1').value;
-    var color_content2=document.getElementById('color_content2').value;
-    var color_content3=document.getElementById('color_content3').value;
+  var place=document.getElementById('place');
+  var headline=document.getElementById('headline');
+  var content1=document.getElementById('content1');
+  var content2=document.getElementById('content2');
+  var content3=document.getElementById('content3');
+  var content4=document.getElementById('content4');
+  var ad_contact=document.getElementById('ad_contact_no');
+  var date=document.getElementById('date');
+  
+  var color_place=document.getElementById('color_place').value;
+  var headline_color=document.getElementById('color_headline').value;
+  var color_content1=document.getElementById('color_content1').value;
+  var color_content2=document.getElementById('color_content2').value;
+  var color_content3=document.getElementById('color_content3').value;
+  var color_content4=document.getElementById('color_content4').value;
+  var color_contact=document.getElementById('color_contact').value;
+  var color_date=document.getElementById('color_date').value;
 
-    headline.innerHTML=headlinedata;
-    headline.style.color=headline_color;
+  place.innerHTML=placedata;
+  place.style.color=color_place;
 
-    content1.innerHTML=contentdata1+" ";
-    content1.style.color = color_content1;
+  headline.innerHTML=headlinedata;
+  headline.style.color=headline_color;
+  
+  content1.innerHTML=contentdata1+" ";
+  content1.style.color = color_content1;
+  
+  content2.innerHTML=contentdata2+" ";
+  content2.style.color = color_content2;
+  
+  content3.innerHTML=contentdata3+" ";
+  content3.style.color = color_content3;
+  
+  content4.innerHTML=contentdata4;
+  content4.style.color = color_content4;
 
-    content2.innerHTML=contentdata2+" ";
-    content2.style.color = color_content2;
+  ad_contact.innerHTML=contactdata;
+  ad_contact.style.color = color_contact;
 
-    content3.innerHTML=contentdata3;
-    content3.style.color = color_content3;
-    
-    // tagline.innerHTML=taglinedata;
-    // tagline.style.color = color_tagline;
-}
-function add_ad_image(){
-    var ad_image = document.getElementById('ad_image');
-    var ad = document.getElementById('ad');
-
-    var file = ad_image.files[0];
-    if (file) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-        var img = new Image();
-        img.src = e.target.result;
-        img.style.maxWidth = '100%';
-        img.style.maxHeight = '100%';
-        ad.innerHTML = '';
-        ad.appendChild(img);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      alert('Please select an image first.');
-    }
-}
-
-function template_bg(){
-    var content_image = document.getElementById('content_image');
-    var template_1 = document.getElementById('template_1');
-
-    var imfile = content_image.files[0];
-    imfile.addEventListener('change', function() {
-        const image = file.files[0];
-        const bgUrl = URL.createObjectUrl(image);
-        template_1.style.backgroundImage = `url(${bgUrl})`;
-    });
+  date.innerHTML=datedata;
+  date.style.color = color_date;
 }
 
-function preview(){
-    content();
-    add_ad_image();
-    template_bg();
-}
 
 function extract(template_im){
   html2canvas(document.getElementById(template_im)).then(function (canvas) {
@@ -80,26 +63,14 @@ function extract(template_im){
   });
 }
 
+function main_image(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-function displayImage() {
-    const input = document.getElementById('image-input');
-    const container = document.getElementById('image-container');
+    reader.onload = function (e) {
+      $('#main_image').attr('src', e.target.result);
+    };
 
-    const file = input.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        const img = new Image();
-        img.src = e.target.result;
-        img.style.maxWidth = '100%';
-        img.style.maxHeight = '100%';
-        container.innerHTML = '';
-        container.appendChild(img);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      alert('Please select an image first.');
-    }
+    reader.readAsDataURL(input.files[0]);
   }
-
-
+}
